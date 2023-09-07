8085e2ff13b5bbc2c4b05fb4ddba7f24c20c08fe
RadiantCrow77 <katherinebobo@hotmail.com>
2023-09-06 21:46:45 -0600

    add main app file and POM dependencies

---
+++ src/main/java/rocketleagueserver/RLApplication.java
@@ -0,0 +1,13 @@
+package rocketleagueserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RLApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RLApplication.class, args);
+	}
+
+}
